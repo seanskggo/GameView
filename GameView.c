40c3f8d619be6b31f182b95b7a7fcc0f50be6a36
Francis <f2002nguyen@gmail.com>
2020-07-23 22:05:25 +1000

    Update GameView.c
    changed PlayerRepUpdate()

--- GameView.c
+++ GameView.c
@@ -289,14 +289,9 @@ static void calcGameState(GameView Gv, char *pastPlays) {
 			// Update current player based on the current play
 			calcCurrPlayer(Gv, currPlay);
 
-			// Update current player's trail
-			PlayerRepUpdatePlayerTrail(Gv->players[Gv->currentPlayer],
-				currPlay
-			);
+			// Update current player's trail/move history
+			PlayerRepUpdate(Gv->players[Gv->currentPlayer], currPlay);
 			
-			PlayerRepUpdateMoveHistory(Gv->players[Gv->currentPlayer],
-				currPlay
-			);
 			processEncounterLocations(Gv, currPlay);
 
 			// Reset currPlayCounter cos we just processed a whole play
