9060e5a966e9e7b6515f22631ff9b6f351e236cc
sschreyer <67688408+sschreyer@users.noreply.github.com>
2020-07-28 17:36:39 +1000

    Sam Wrote HvNew()

--- HunterView.c
+++ HunterView.c
@@ -21,10 +21,34 @@
 #include "Places.h"
 // add your own #includes here
 
-// TODO: ADD YOUR OWN STRUCTS HERE
+#define NUM_PLAYERS 4
+
+// Character struct for storing character information
+typedef struct character {
+	int health;
+	int numMoves;
+	PlaceId *moves;
+	bool canFreeMoves;
+} Character;
+
+// prolly not necessary
+/*
+typedef struct location {
+	int traps;
+	bool vamp;
+} Location; */
 
 struct hunterView {
-	// TODO: ADD FIELDS HERE
+	int round;
+	int score;
+	GameView gv;
+	Player current;
+	//Location *places;
+	Character *hunter;
+	PlaceId *revealedDracMovesHistory;
+	int numDracMovesHistory;
+	bool canFreeDracHistory;
+	Map map;
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -32,13 +56,32 @@ struct hunterView {
 
 HunterView HvNew(char *pastPlays, Message messages[])
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
 	HunterView new = malloc(sizeof(*new));
 	if (new == NULL) {
 		fprintf(stderr, "Couldn't allocate HunterView!\n");
 		exit(EXIT_FAILURE);
 	}
 
+	new->gv = GvNew(pastPlays, messages);
+	new->round = GvGetRound(new->gv);
+	new->score = GvGetScore(new->gv);
+	new->current = GvGetPlayer(new->gv);
+
+	new->hunter = malloc(5 * sizeof(*new->hunter));
+
+	// initialise hunters
+	for (int i = 0; i < NUM_PLAYERS; i++) {
+		new->hunter[i].health = GvGetHealth(new->gv, i);
+		new->hunter[i].moves = GvGetMoveHistory(new->gv, i,
+			&new->hunter[i].numMoves, &new->hunter[i].canFreeMoves);
+	}
+	// The use of "tempB" might be slightly problematic/bad style.
+	// I will keep it for now.
+	// the alternative is to store whether you can free this info in
+	// the above structs. Same for ""
+	new->revealedDracMovesHistory = GvGetMoveHistory(new->gv, PLAYER_DRACULA,
+	                          &new->numDracMovesHistory,
+							  &new->canFreeDracHistory);
 	return new;
 }
 
